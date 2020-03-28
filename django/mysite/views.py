a40339f56cc9581ffdb19b28225c89203e3bd8dc
LAI, WEI-CHEN <kill630459@gmail.com>
2020-03-28 16:46:37 +0800

    templates統一成英文

--- django/mysite/views.py
+++ django/mysite/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
-from django.contrib.auth.forms import UserCreationForm  # 新增
+from django.contrib.auth.forms import UserCreationForm
 
 
 def register(request):
